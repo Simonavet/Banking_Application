157e9f451b6f1dd86178c6f11806fc29ad0e8063
Simonavet <simona.hassanein@gmail.com>
2023-05-13 20:44:30 +0300

    second commit

--- the_app/operating_system.py
+++ the_app/operating_system.py
@@ -6,7 +6,7 @@ account2 = Bank_account('John Wick', 'EUR67890')
 account1.generate_pin()
 print(f'Enter your PIN: '), account1.account_activation(pin=int(input()))
 print(f'Enter the amount that you want to supply: '), account1.account_supply(amount=int(input()))
-account1.card_payment(100)
+print(f'Enter the amount you want to pay: '), account1.card_payment(amount=int(input()))
 account1.balance_query()
 
 account2.generate_pin()
@@ -14,3 +14,4 @@ print(f'Enter your PIN: '), account2.account_activation(pin=int(input()))
 print(f'Enter the amount that you want to supply: '), account2.account_supply(amount=int(input()))
 print(f'Enter the amount you want to pay: '), account2.card_payment(amount=int(input()))
 account2.balance_query()
+
